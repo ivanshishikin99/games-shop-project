702957cbcbb3eb9b17532ae4c89ff63094299e77
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-06-09 13:02:58 +0300

    added jwt authentication, database and general settings config models to the config file

---
+++ core/config.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+
+from pydantic import BaseModel, PostgresDsn
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+BASE_DIR = Path(__file__).parent.parent
+
+class DbConfig(BaseModel):
+    url: PostgresDsn
+    echo: bool = False
+    echo_pool: bool = False
+    pool_size: int = 50
+    max_overflow: int = 10
+    naming_conventions: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
+
+class AuthJWT(BaseModel):
+    algorithm: str = 'RS256'
+    access_token_expire_minutes: int = 5
+    refresh_token_expire_minutes: int = 42600
+    public_key_path: Path = BASE_DIR / 'certs' / 'public_key.pem'
+    private_key_path: Path = BASE_DIR / 'certs' / 'private_key.pem'
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file='.env',
+                                      env_prefix='APP_CONFIG__',
+                                      env_nested_delimiter='__',
+                                      case_sensitive=False)
+    db: DbConfig
+    auth_jwt: AuthJWT = AuthJWT()
+
+settings = Settings()
