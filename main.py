17c381505583a3cfbbd6c623b0224d4ca03747a5
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-06-10 10:52:49 +0300

    created an api_v1 router and connected it to the main app

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from contextlib import asynccontextmanager
-
+from api_v1.views import router as api_v1_router
 import uvicorn
 from fastapi import FastAPI
-
 from utils.db_helper import db_helper
 
 
@@ -12,6 +11,8 @@ async def lifespan(app: FastAPI):
     await db_helper.dispose()
 
 app = FastAPI(lifespan=lifespan)
+
+app.include_router(router=api_v1_router)
 
 if __name__ == '__main__':
     uvicorn.run('main:app', reload=True)
