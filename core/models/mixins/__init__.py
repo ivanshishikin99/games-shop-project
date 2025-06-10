5e811da21ab862bb9c4709ead58bac936ee18840
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-06-10 10:34:44 +0300

    created a mixins package with id, updated at and created at mixins

---
+++ core/models/mixins/__init__.py
@@ -0,0 +1,8 @@
+__all__ = ('IdIntPkMixin',
+           'CreatedAtMixin',
+           'UpdatedAtMixin',
+           )
+
+from .id_int_pk import IdIntPkMixin
+from .created_at_mixin import CreatedAtMixin
+from .updated_at_mixin import UpdatedAtMixin
