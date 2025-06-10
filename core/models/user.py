0fcc896fcb1331917b0f8786bce8dfc2096042ee
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-06-10 10:44:00 +0300

    created a user model

---
+++ core/models/user.py
@@ -0,0 +1,19 @@
+from datetime import date
+
+from sqlalchemy.orm import Mapped, mapped_column
+from core.models.mixins import IdIntPkMixin, CreatedAtMixin, UpdatedAtMixin
+from core.models import Base
+
+
+class User(Base, IdIntPkMixin, CreatedAtMixin, UpdatedAtMixin):
+    username: Mapped[str] = mapped_column(nullable=False, unique=True)
+    password: Mapped[bytes] = mapped_column(nullable=False)
+    email: Mapped[str] = mapped_column(nullable=True, unique=True)
+    first_name: Mapped[str] = mapped_column(nullable=True)
+    last_name: Mapped[str] = mapped_column(nullable=True)
+    country: Mapped[str] = mapped_column(nullable=True)
+    phone_number: Mapped[str] = mapped_column(nullable=True)
+    sex: Mapped[str] = mapped_column(nullable=True)
+    verified: Mapped[bool] = mapped_column(nullable=False)
+    role_access: Mapped[str] = mapped_column(nullable=False)
+    date_of_birth: Mapped[date] = mapped_column(nullable=True)
