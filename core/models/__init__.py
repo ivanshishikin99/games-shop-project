42b2fc1e664f2ceb7c585653ebf45a69f29fa3af
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-06-11 23:36:20 +0300

    added a games model

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,9 +1,10 @@
 __all__ = ('Base',
            'User',
            'VerificationToken',
-
+           'Game',
            )
 
 from .base import Base
 from .user import User
 from .verification_token import VerificationToken
+from .game import Game
---
+++ core/models/game.py
@@ -0,0 +1,16 @@
+from datetime import date
+
+from sqlalchemy.orm import Mapped, mapped_column
+
+from core.models import Base
+from core.models.mixins import IdIntPkMixin, CreatedAtMixin, UpdatedAtMixin
+
+
+class Game(Base, IdIntPkMixin, CreatedAtMixin, UpdatedAtMixin):
+    name: Mapped[str] = mapped_column(nullable=False)
+    price: Mapped[int] = mapped_column(nullable=False)
+    date_of_release: Mapped[date] = mapped_column(nullable=False)
+    age_censor: Mapped[int] = mapped_column(nullable=False)
+    developer: Mapped[str] = mapped_column(nullable=False)
+    rating: Mapped[int] = mapped_column(nullable=False)
+    genre: Mapped[str] = mapped_column(nullable=False)
