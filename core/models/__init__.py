4a8f8497d2336df0f5894a0eba7387a0853cb018
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-06-11 22:19:59 +0300

    .

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,6 +1,9 @@
 __all__ = ('Base',
            'User',
+           'VerificationToken',
+
            )
 
 from .base import Base
 from .user import User
+from .verification_token import VerificationToken
