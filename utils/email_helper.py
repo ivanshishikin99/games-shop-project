c0bcd3ab21762073ade7bee19ff2ba1c9ec4de1c
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-06-11 16:51:27 +0300

    created an email helper file

---
+++ utils/email_helper.py
@@ -0,0 +1,18 @@
+from email.message import EmailMessage
+
+import aiosmtplib
+
+async def send_email(recipient: str,
+                     subject: str,
+                     body: str):
+    admin_email = 'games_shop@mail.ru'
+    message = EmailMessage()
+    message['From'] = admin_email
+    message['To'] = recipient
+    message['Subject'] = subject
+    message.set_content(body)
+    await aiosmtplib.send(message,
+                          sender=admin_email,
+                          recipients=[recipient],
+                          hostname='localhost',
+                          port=1025)
