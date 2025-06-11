3636f15c9f37d5e4b377142a4d47f5e44b0a677a
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-06-11 16:49:23 +0300

    added a partial user info update view with oauth2 implemented

--- api_v1/users/views.py
+++ api_v1/users/views.py
@@ -2,15 +2,15 @@ from fastapi import APIRouter, status, Depends, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from api_v1.users.crud import create_user, login_user
-from api_v1.users.schemas import UserRead, UserCreate
+from api_v1.users.crud import create_user, login_user, update_user
+from api_v1.users.schemas import UserRead, UserCreate, UserUpdate
 from core.models import User
 from utils.db_helper import db_helper
 from utils.token_helpers import TokenModel, create_access_token, create_refresh_token, get_user_by_token
 
 router = APIRouter(prefix='/users', tags=['Users'])
 
-@router.post('/register_user', response_model=UserRead, response_model_exclude_none=True, status_code=status.HTTP_202_ACCEPTED)
+@router.post('/register_user', response_model=UserRead, response_model_exclude_none=True, status_code=status.HTTP_201_CREATED)
 async def register_user_view(user: UserCreate, session: AsyncSession = Depends(db_helper.session_getter)) -> User:
     return await create_user(user=user, session=session)
 
@@ -25,3 +25,7 @@ async def login_user_view(form_data: OAuth2PasswordRequestForm = Depends(), sess
 @router.get('/get_user_info', response_model=UserRead, response_model_exclude_none=True, status_code=status.HTTP_200_OK)
 async def get_user_info_view(user: User = Depends(get_user_by_token)) -> User | HTTPException:
     return user
+
+@router.patch('/update_user_info', response_model=UserRead, response_model_exclude_none=True, status_code=status.HTTP_202_ACCEPTED)
+async def update_user_info_view(user_info: UserUpdate, session: AsyncSession = Depends(db_helper.session_getter), user: User = Depends(get_user_by_token)) -> User | HTTPException:
+    return await update_user(user_to_update=user, user_info=user_info, session=session)
