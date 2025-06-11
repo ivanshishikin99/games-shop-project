7e96cfc57305c7fb54b2299cdb97192d83c5967b
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-06-12 00:54:16 +0300

    added a get game by id dependency

---
+++ api_v1/games/dependencies.py
@@ -0,0 +1,13 @@
+from fastapi import Depends, HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from api_v1.games.crud import get_game_by_id
+from core.models import Game
+from utils.db_helper import db_helper
+
+
+async def get_game_by_id_dependency(game_id: int, session: AsyncSession = Depends(db_helper.session_getter)) -> Game | HTTPException:
+    game = await get_game_by_id(game_id=game_id, session=session)
+    if not game:
+        return HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    return game
