ae2f849a73f49c2dc451d16e839e9aede6c9eb1e
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-06-11 16:27:32 +0300

    added a user login operation

--- api_v1/users/crud.py
+++ api_v1/users/crud.py
@@ -1,8 +1,10 @@
+from fastapi import HTTPException, status
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from api_v1.users.schemas import UserCreate
 from core.models import User
-from utils.password_helpers import hash_password
+from utils.password_helpers import hash_password, verify_password
 
 
 async def create_user(user: UserCreate, session: AsyncSession) -> User:
@@ -15,4 +17,14 @@ async def create_user(user: UserCreate, session: AsyncSession) -> User:
 
 async def get_user_by_id(user_id: int, session: AsyncSession) -> User | None:
     user = await session.get(User, user_id)
+    return user
+
+async def login_user(username: str, password: str, session: AsyncSession) -> User | HTTPException:
+    statement = select(User).where(User.username == username)
+    user = await session.execute(statement)
+    user = user.scalar_one()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Wrong username.')
+    if not verify_password(password=password, hashed_password=user.password):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Wrong password.')
     return user
