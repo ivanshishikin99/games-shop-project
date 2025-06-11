b24ef6e0abde625a9c2a833e00752b01da4302b4
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-06-11 23:43:38 +0300

    created user schemas

---
+++ api_v1/games/schemas.py
@@ -0,0 +1,40 @@
+from datetime import date
+
+from pydantic import BaseModel
+
+
+class GameCreate(BaseModel):
+    name: str
+    price: int
+    date_of_release: date
+    age_censor: int
+    developer: str
+    rating: int
+    genre: str
+
+class GameRead(BaseModel):
+    name: str
+    price: int
+    date_of_release: date
+    age_censor: int
+    developer: str
+    rating: int
+    genre: str
+
+class GameUpdatePartial(BaseModel):
+    name: str | None = None
+    price: int | None = None
+    date_of_release: date | None = None
+    age_censor: int | None = None
+    developer: str | None = None
+    rating: int | None = None
+    genre: str | None = None
+
+class GameUpdateFull(BaseModel):
+    name: str
+    price: int
+    date_of_release: date
+    age_censor: int
+    developer: str
+    rating: int
+    genre: str
