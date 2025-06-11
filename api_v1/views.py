9966af9b6feaf40c849a5af14d8574f99832507d
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-06-11 23:38:55 +0300

    created a games package

--- api_v1/views.py
+++ api_v1/views.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter
 from api_v1.users.views import router as users_router
+from api_v1.games.views import router as games_router
 
 router = APIRouter(prefix='/api_v1')
 
 router.include_router(users_router)
+
+router.include_router(games_router)
