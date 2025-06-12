d684409b97de34bce3cc429657f41cf2233346e3
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-06-12 10:38:06 +0300

    added superuser router to the api_v1 router

--- api_v1/views.py
+++ api_v1/views.py
@@ -1,9 +1,12 @@
 from fastapi import APIRouter
 from api_v1.users.views import router as users_router
 from api_v1.games.views import router as games_router
+from api_v1.superuser.views import router as superuser_router
 
 router = APIRouter(prefix='/api_v1')
 
 router.include_router(users_router)
 
 router.include_router(games_router)
+
+router.include_router(superuser_router)
