aaba1e58fd624453fdeaa5a4256d2e4088d470ec
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-06-13 22:49:31 +0300

    split the send mail function into two separate functions for background task convenience

--- utils/email_helper.py
+++ utils/email_helper.py
@@ -3,6 +3,10 @@ from email.message import EmailMessage
 
 import aiosmtplib
 
+from api_v1.users import crud
+from core.models import User
+from utils.db_helper import db_helper
+
 
 async def send_email(recipient: str,
                      subject: str,
@@ -22,3 +26,10 @@ async def send_email(recipient: str,
 
 def generate_secret_verification_code():
     return uuid.uuid4()
+
+async def send_email_dependency(user_id: int, secret_code: str):
+    async with db_helper.session_maker() as session:
+        user: User = await crud.get_user_by_id(user_id=user_id, session=session)
+    await send_email(recipient=user.email,
+                     subject='Email verification',
+                     body=f"Your verification code is {secret_code}. If this e-mail was sent by mistake just ignore it.")
