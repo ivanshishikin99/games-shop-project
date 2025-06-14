8265efa337dfeff06c27b2c2049c5e05c5be74f3
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-06-14 10:50:09 +0300

    added a logging config

--- core/config.py
+++ core/config.py
@@ -1,9 +1,28 @@
+import logging
 from pathlib import Path
+from typing import Literal
 
 from pydantic import BaseModel, PostgresDsn
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 BASE_DIR = Path(__file__).parent.parent
+
+LOG_DEFAULT_FORMAT = ("[%(asctime)s.%(msecs)03d] %(module)10s:%(lineno)-3d %(levelname)-7s - %(message)s")
+
+
+class LoggingConfig(BaseModel):
+    log_level: Literal[
+        "debug",
+        "info",
+        "warning",
+        "error",
+        "critical",
+    ] = "info"
+    log_format: str = LOG_DEFAULT_FORMAT
+
+    @property
+    def log_level_value(self) -> int:
+        return logging.getLevelNamesMapping()[self.log_level.upper()]
 
 class DbConfig(BaseModel):
     url: PostgresDsn
