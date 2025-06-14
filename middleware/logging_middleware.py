b48af3353069ce22c027f1808581b208670abe38
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-06-14 13:40:42 +0300

    print statement changed to a logger and processing time changed to time.perf_counter

--- middleware/logging_middleware.py
+++ middleware/logging_middleware.py
@@ -1,18 +1,24 @@
 import logging
 import time
+from typing import Callable, Awaitable
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 from fastapi.requests import Request
+
+from core.config import settings
+
+logging.basicConfig(level=settings.logging_config.log_level,
+                    format=settings.logging_config.log_format)
+
+log = logging.getLogger(__name__)
 
 
 def register_middleware(app: FastAPI):
     logger = logging.getLogger("uvicorn.access")
     logger.disabled = True
     @app.middleware("http")
-    async def custom_logging(request: Request, call_next):
-        start_time = time.time()
-        response = await call_next(request)
-        processing_time = time.time() - start_time
-        message = f"{request.client.host}:{request.client.port} - {request.method} - {request.url.path} - {response.status_code} completed after {processing_time} s."
-        print(message)
-        return response
+    async def custom_logging(request: Request, call_next: Callable[[Request], Awaitable[Response]]) -> Response:
+        now = time.perf_counter()
+        result = await call_next(request)
+        log.info("Request %s to %s took %s seconds.", request.method, request.url, time.perf_counter() - now)
+        return result
