694a3baa330ac1fc240b7e6838e2320fd8a208ba
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-06-15 15:19:52 +0300

    processing time is now stored in headers

--- middleware/logging_middleware.py
+++ middleware/logging_middleware.py
@@ -20,5 +20,7 @@ def register_middleware(app: FastAPI):
     async def custom_logging(request: Request, call_next: Callable[[Request], Awaitable[Response]]) -> Response:
         now = time.perf_counter()
         result = await call_next(request)
-        log.info("Request %s to %s took %s seconds.", request.method, request.url, time.perf_counter() - now)
+        log.info("Request %s to %s", request.method, request.url)
+        process_time = time.perf_counter() - now
+        result.headers["X-Proccess-Time"] = f"{process_time:.5f}"
         return result
