e592f925a55067e006019f66788e64402c7fb70d
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-06-17 11:40:32 +0300

    registered error handlers

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from contextlib import asynccontextmanager
 from api_v1.views import router as api_v1_router
 import uvicorn
 from fastapi import FastAPI
-
-from middleware.logging_middleware import register_middleware
+from error_handlers import register_error_handlers
+from middleware.register_middleware import register_middleware
 from utils.db_helper import db_helper
 
 
@@ -13,6 +13,8 @@ async def lifespan(app: FastAPI):
     await db_helper.dispose()
 
 app = FastAPI(lifespan=lifespan, title='Games shop')
+
+register_error_handlers(app=app)
 
 register_middleware(app=app)
 
