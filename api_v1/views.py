3e823959e57390194fc4318adfbad0c73f4e1b5e
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-06-17 11:40:44 +0300

    registered the service router

--- api_v1/views.py
+++ api_v1/views.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from api_v1.users.views import router as users_router
 from api_v1.games.views import router as games_router
 from api_v1.superuser.views import router as superuser_router
+from api_v1.service import router as service_router
 
 router = APIRouter(prefix='/api_v1')
 
@@ -10,3 +11,5 @@ router.include_router(users_router)
 router.include_router(games_router)
 
 router.include_router(superuser_router)
+
+router.include_router(service_router)
