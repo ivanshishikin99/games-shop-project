e3f1a8426ce7d4e89052164037206cbb54886dd2
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-06-20 12:15:21 +0300

    created a function that deletes expired verification tokens every hour

---
+++ utils/delete_expired_verification_tokens.py
@@ -0,0 +1,25 @@
+import asyncio
+import logging
+from datetime import datetime, timedelta
+
+from sqlalchemy import select
+
+from core.config import settings
+from core.models.verification_token import VerificationToken
+from utils.db_helper import db_helper
+
+
+async def delete_tokens(interval: int = 3600):
+    logging.basicConfig(level=settings.logging_config.log_level,
+                        format=settings.logging_config.log_format)
+    log = logging.getLogger()
+    while True:
+        async with db_helper.session_maker() as session:
+            statement = select(VerificationToken).where(datetime.now() - VerificationToken.created_at > timedelta(hours=1))
+            tokens = await session.execute(statement)
+            tokens = tokens.scalars().all()
+            for i in tokens:
+                await session.delete(i)
+                await session.commit()
+            log.info('Verification table has been cleaned.')
+        await asyncio.sleep(delay=interval)
