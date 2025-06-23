f817650823ecf64a8463dedd6d5d31d29b4ab5c8
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-06-23 23:29:22 +0300

    added genres router to the master router

--- api_v1/views.py
+++ api_v1/views.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from api_v1.users.views import router as users_router
 from api_v1.games.views import router as games_router
 from api_v1.superuser.views import router as superuser_router
+from api_v1.genres.views import router as genres_router
 from api_v1.service import router as service_router
 
 
@@ -15,3 +16,5 @@ router.include_router(games_router)
 router.include_router(superuser_router)
 
 router.include_router(service_router)
+
+router.include_router(genres_router)
