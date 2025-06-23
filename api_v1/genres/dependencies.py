ee11fa55e2ad6236ff83bf5c76cadcfdf7a15834
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-06-23 23:28:43 +0300

    created a get genre by id dependency

---
+++ api_v1/genres/dependencies.py
@@ -0,0 +1,12 @@
+from fastapi import Depends, HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from api_v1.genres.crud import get_genre_by_id
+from core.models import Genre
+from utils.db_helper import db_helper
+
+
+async def get_genre_by_id_dependency(genre_id: int, session: AsyncSession = Depends(db_helper.session_getter)) -> Genre | HTTPException:
+    if not (genre := await get_genre_by_id(genre_id=genre_id, session=session)):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Genre not found.")
+    return genre
