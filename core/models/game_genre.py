9403dc36e59c5070ea262ff5dba300f9c1804c69
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-06-23 11:49:05 +0300

    created a secondary game-genre association table for m2m relationship

---
+++ core/models/game_genre.py
@@ -0,0 +1,13 @@
+from typing import TYPE_CHECKING
+from sqlalchemy import Table, Column, ForeignKey
+from .base import Base
+
+if TYPE_CHECKING:
+    from core.models.game import Game
+    from core.models.genre import Genre
+
+game_genre_association_table = Table("game_genre_association",
+                               Base.metadata,
+                               Column("game_id", ForeignKey("game.id"), primary_key=True),
+                               Column("genre_id", ForeignKey("genre.id"), primary_key=True),
+                               )
