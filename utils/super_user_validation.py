49e9a07969eb2bd64244f4a8b9d2d104ba07c91d
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-06-24 12:09:37 +0300

    created a super_user_validation function in accordance with DRY

---
+++ utils/super_user_validation.py
@@ -0,0 +1,10 @@
+from fastapi import HTTPException, status
+
+from core.models import User
+
+
+def super_user_validate(user: User) -> bool | HTTPException:
+    if not user.role_access == 'Super user':
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail='You are not authorized to view this page.')
+    return True
