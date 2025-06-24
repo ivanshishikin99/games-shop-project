7e07962036111efa3d92686a504dbe28330402b4
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-06-24 12:12:12 +0300

    role access is now validated through a super user validate function

--- api_v1/genres/views.py
+++ api_v1/genres/views.py
@@ -5,6 +5,7 @@ from api_v1.genres.crud import create_genre, update_genre, delete_genre
 from api_v1.genres.dependencies import get_genre_by_id_dependency
 from api_v1.genres.schemas import GenreCreate, GenreRead, GenreUpdate
 from core.models import User, Genre
+from utils import super_user_validate
 from utils.db_helper import db_helper
 from utils.token_helpers import get_user_by_token
 
@@ -15,35 +16,41 @@ router = APIRouter(prefix='/genres', tags=['Genres'])
 async def create_genre_view(genre: GenreCreate,
                        session: AsyncSession = Depends(db_helper.session_getter),
                        user: User = Depends(get_user_by_token)) -> Genre:
-    if not user.role_access == "Super user":
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                            detail='You are not authorized to view this page.')
-    return await create_genre(genre=genre,
-                              session=session)
+    if super_user_validate(user=user):
+        return await create_genre(genre=genre,
+                                  session=session)
 
 
 @router.get('/get_genre_by_id', response_model=GenreRead, response_model_exclude_none=True)
 async def get_genre_by_id_view(genre_id: int,
-                               genre: Genre = Depends(get_genre_by_id_dependency)) -> Genre:
-    return genre
+                               genre: Genre = Depends(get_genre_by_id_dependency),
+                               user: User = Depends(get_user_by_token)) -> Genre:
+    if super_user_validate(user=user):
+        return genre
 
 
 @router.get('/get_games_by_genre_id')
 async def get_games_by_genre_id_view(genre_id: int,
-                                     genre: Genre = Depends(get_genre_by_id_dependency)):
-    return genre.games
+                                     genre: Genre = Depends(get_genre_by_id_dependency),
+                                     user: User = Depends(get_user_by_token)):
+    if super_user_validate(user=user):
+        return genre.games
 
 
 @router.patch('/update_genre', response_model=GenreRead, response_model_exclude_none=True)
 async def update_genre_view(genre_id: int,
                             genre_new: GenreUpdate,
                             session: AsyncSession = Depends(db_helper.session_getter),
-                            genre: Genre = Depends(get_genre_by_id_dependency)) -> Genre:
-    return await update_genre(genre=genre, new_genre=genre_new, session=session)
+                            genre: Genre = Depends(get_genre_by_id_dependency),
+                            user: User = Depends(get_user_by_token)) -> Genre:
+    if super_user_validate(user=user):
+        return await update_genre(genre=genre, new_genre=genre_new, session=session)
 
 @router.delete('/delete_genre')
 async def delete_genre_view(genre_id: int,
                             genre: Genre = Depends(get_genre_by_id_dependency),
-                            session: AsyncSession = Depends(db_helper.session_getter)):
-    return await delete_genre(genre=genre, session=session)
+                            session: AsyncSession = Depends(db_helper.session_getter),
+                            user: User = Depends(get_user_by_token)):
+    if super_user_validate(user=user):
+        return await delete_genre(genre=genre, session=session)
 
