38e6c6630b1a2e98348fc54b7a118e5b32a981f8
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-06-26 17:17:28 +0300

    included the reviews router

--- api_v1/views.py
+++ api_v1/views.py
@@ -4,6 +4,7 @@ from api_v1.users.views import router as users_router
 from api_v1.games.views import router as games_router
 from api_v1.superuser.views import router as superuser_router
 from api_v1.genres.views import router as genres_router
+from api_v1.reviews.views import router as reviews_router
 from api_v1.service import router as service_router
 
 
@@ -18,3 +19,5 @@ router.include_router(superuser_router)
 router.include_router(service_router)
 
 router.include_router(genres_router)
+
+router.include_router(reviews_router)
