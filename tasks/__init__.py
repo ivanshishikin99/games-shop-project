154aad0655414a4b0bb9f3b40d653cb576cd65da
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-06-26 17:15:23 +0300

    logging level is now logging.INFO

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -8,5 +8,5 @@ from core.config import settings
 from .welcome_email_notification import send_welcome_email
 
 if sys.argv[0] == 'worker':
-    logging.basicConfig(level=settings.logging_config.log_level_value,
+    logging.basicConfig(level=logging.INFO,
                         format=settings.logging_config.log_format)
