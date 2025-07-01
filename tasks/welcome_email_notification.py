cbbe4735bf4f65c25c394e5a4847d6833e656291
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-07-01 12:30:26 +0300

    deleted welcome_email_notification file

--- tasks/welcome_email_notification.py
+++
@@ -1,9 +0,0 @@
-from logger import log
-from core.taskiq_broker import broker
-from mailing.email_helper import send_welcome_email as send
-
-
-@broker.task
-async def send_welcome_email(user_id: int) -> None:
-    log.info("Sending welcome email to user %s", user_id)
-    await send(user_id=user_id)
