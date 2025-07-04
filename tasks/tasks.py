8e19c8660fcac793ffde4ec0778f4081f86968d8
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-07-04 12:25:08 +0300

    send welcome email and send email verification tasks are now retried upon failure

--- tasks/tasks.py
+++ tasks/tasks.py
@@ -1,22 +1,33 @@
-from logger import log
 from mailing.email_helper import send_welcome_email as send
 from mailing.email_helper import send_email
 from tasks.celery import celery
 
+from logger import log
 
-@celery.task
-def send_welcome_email(user_id: int,
+
+@celery.task(bind=True, max_retries=5)
+def send_welcome_email(self,
+                       user_id: int,
                        user_email: str,
                        user_username: str):
-    log.info("Sending welcome email to user %s", user_id)
-    return send(user_email=user_email,
-                user_username=user_username)
+    try:
+        log.info("Sending welcome email to user with id: %s", user_id)
+        return send(user_email=user_email,
+                    user_username=user_username)
+    except:
+        self.retry()
 
 
-@celery.task
-def send_email_verification_code(user_email: str,
+@celery.task(bind=True, max_retries=5)
+def send_email_verification_code(self,
+                                 user_id: int,
+                                 user_email: str,
                                  secret_code: str):
-    return send_email(recipient=user_email,
-                      subject='Email verification',
-                      body=f"Your verification code is {secret_code}. If this e-mail was sent by mistake just ignore it."
-                      )
+    try:
+        log.info("Sending verification code to user with id: %s", user_id)
+        return send_email(recipient=user_email,
+                          subject='Email verification',
+                          body=f"Your verification code is {secret_code}. If this e-mail was sent by mistake just ignore it."
+                          )
+    except:
+        self.retry()
