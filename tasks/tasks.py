96ed5a8267397a3199fd2311556e1de3b9d29d9c
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-07-04 10:55:21 +0300

    email verification code sending function is now a celery task

--- tasks/tasks.py
+++ tasks/tasks.py
@@ -1,5 +1,6 @@
 from logger import log
 from mailing.email_helper import send_welcome_email as send
+from mailing.email_helper import send_email
 from tasks.celery import celery
 
 
@@ -10,3 +11,12 @@ def send_welcome_email(user_id: int,
     log.info("Sending welcome email to user %s", user_id)
     return send(user_email=user_email,
                 user_username=user_username)
+
+
+@celery.task
+def send_email_verification_code(user_email: str,
+                                 secret_code: str):
+    return send_email(recipient=user_email,
+                      subject='Email verification',
+                      body=f"Your verification code is {secret_code}. If this e-mail was sent by mistake just ignore it."
+                      )
