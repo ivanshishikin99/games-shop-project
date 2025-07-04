a480c87737100e81d4b4b0d804997800e28f79ac
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-07-04 12:24:39 +0300

    celery config parameters are now sent through an update function

--- tasks/celery.py
+++ tasks/celery.py
@@ -4,4 +4,11 @@ from core.config import settings
 
 celery = Celery("tasks.celery",
                 broker=f"{settings.celery_config.backend}://{settings.celery_config.hostname}:{settings.celery_config.port}//",
-                backend='rpc://')
+                backend='redis://')
+
+celery.conf.update(
+    result_backend=settings.celery_config.result_backend,
+    broker_transport_options={'visibility_timeout': settings.celery_config.visibility_timeout},
+    task_serializer=settings.celery_config.task_serializer,
+    timezone=settings.celery_config.timezone
+)
